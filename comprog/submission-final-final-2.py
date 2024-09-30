d9d04e85f8b56f3ff5dc9ecc3098502eb79e2f7d
Tyron Franzke <131623486+tyronfranzke@users.noreply.github.com>
2024-10-01 00:16:47 +0200

    Aufgaben für 2024 erweitert.

---
+++ comprog/submission-final-final-2.py
@@ -0,0 +1,16 @@
+def is_prime(n):
+    if n < 2:
+        return False
+    
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    
+    return True
+
+n = int(input("Enter a number: "))
+
+if is_prime(n):
+    print(f"{n} is a prime number.")
+else:
+    print(f"{n} is not a prime number.")
